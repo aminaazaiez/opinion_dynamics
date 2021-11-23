996fbf22ca3fc466d22acbd0938421d1220e888c
Romain Reuillon <romain.reuillon@iscpif.fr>
2021-11-23 17:24:27 +0100

    Add network read

--- src/main/scala/Dynamic.scala
+++ src/main/scala/Dynamic.scala
@@ -16,33 +16,43 @@
  */
 
 import scala.util.Random
+import better.files.*
 
 def test = "test"
 
 
-@main def dynamic =
+
+
+@main def dynamic(network: String) =
   val random = new Random(42)
 
-  val nbNodes = 100
 
   val mu = 0.4
   val d = 0.2
 
-  val nodes: Array[Double] = Array.fill(nbNodes)(random.nextDouble())
+  val networkFile = File(network)
+  val nbNodes = networkFile.lines.head.toInt
 
-  type Link = Array[Int]
+   val nodes: Array[Double] = Array.fill(nbNodes)(random.nextDouble())
 
-  //val links: Array[Array[Int]] = Array.fill(300)(Array.fill(2)(random.nextInt(nbNodes)))
-  val links: Array[Link] = Array.fill(300) {
-    Array.fill(2)(random.nextInt(nbNodes))
-  }
+   type Link = Array[Int]
+ //
+//  //val links: Array[Array[Int]] = Array.fill(300)(Array.fill(2)(random.nextInt(nbNodes)))
+//  val links: Array[Link] = Array.fill(300) {
+//    Array.fill(2)(random.nextInt(nbNodes))
+//  }
 
-  val begin = System.currentTimeMillis()
+  val links =
+    networkFile.lines.drop(1).map { l =>
+      l.split(",").map(_.toInt).toArray
+    }.toArray
+
+  //val begin = System.currentTimeMillis()
 
   for
-    s <- 0 until 100000000
+    s <- 0 until 10
   do
-    val l = links(random.nextInt(nbNodes))
+    val l = links(random.nextInt(links.length))
     val i = l(0)
     val j = l(1)
 
@@ -55,6 +65,6 @@ def test = "test"
       nodes(i) = xi + mu * (xj - xi)
       nodes(j) = xj + mu * (xi - xj)
 
-  println(System.currentTimeMillis() - begin)
+  //println(System.currentTimeMillis() - begin)
 
   nodes
